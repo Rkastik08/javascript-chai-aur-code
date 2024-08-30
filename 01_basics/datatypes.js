3ca9d89478db2d43f374d1964ae720ce32f89a40
Rkastik08 <139684202+Rkastik08@users.noreply.github.com>
2024-08-30 17:04:36 +0000

    done!

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -2,3 +2,31 @@
 
 //alert("hello js") //this syntax of alert keyword is now outdated for nodejs. but it can be use in browser till now
 
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint for very big or large number used in stock market websites.
+// string => ""
+// boolean => true/false
+// null => standalone value can be given seperately by simply typing Null by word.it means empty
+// undefined => not defined yet. its not empty but not defined.
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //it will output undefined type in js.
+console.log(typeof null);//it is an object.
