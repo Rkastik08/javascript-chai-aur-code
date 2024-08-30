edddb3524a5589d7fd22665d8cbd33586381326c
Astik08 <139684202+Rkastik08@users.noreply.github.com>
2024-08-30 20:05:57 +0000

    done!

---
+++ 01_basics/04_conversion_operation.js
@@ -0,0 +1,27 @@
+//conversion is nothing but converting the particular type in another variable. calling the previous datatype variable.
+let score = "astik"
+
+let valueInNumber = Number(score);
+console.log(typeof (score));
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+//when a number is converted from string that can easily convert
+// but when 33abc is the string then after convertion showing NaN.
+//for null 0, for undefined undefined itself.
+
+let isLoggedIn = 1
+
+let valueInNumber2 = Boolean(isLoggedIn)
+
+console.log(valueInNumber2);
+
+//in boolean:-
+// 1 => true, "", 0 => false, "astik" => true.
+
+
+
+
+
+
+
