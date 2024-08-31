aa22a7f7fb3b25cb6637648787274c1292fcb3e2
Astik08 <139684202+Rkastik08@users.noreply.github.com>
2024-08-31 08:32:34 +0000

    done!

---
+++ 01_basics/06_datatypes-summary.js
@@ -0,0 +1,38 @@
+// primitive datatypes 
+
+// String, Number, Boolean, null, undefined, BigInt, Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const Id = Symbol('123')// Symbols
+const anotherId = Symbol('123')
+
+console.log(Id === anotherId);
+
+const bigNumber = 83748926394876n //ending with n means bigint
+
+console.log(typeof bigNumber);
+
+//Reference(Non-primitive) datatypes
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman","Nagraj","Doga"] // array
+
+let myObj = {
+    name: "Astik",
+    score: 23,
+    age: 20
+
+} // its called object
+
+
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+} //it is function
+
+console.log(typeof myFunction);
+
