05618bf70d73a9e55d5d65b5294fa2a287c025b4
Astik08 <139684202+Rkastik08@users.noreply.github.com>
2024-09-02 07:10:16 +0000

    done!

--- 01_basics/06_datatypes-summary.js
+++ 01_basics/06_datatypes-summary.js
@@ -37,3 +37,36 @@ const myFunction = function(){
 console.log(typeof myFunction);
 console.log(typeof heros);
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// this is called Heap memory reference(value changed).
+let userOne = {
+    email: "astik@google.com",
+    age: 20
+}
+
+let userTwo = userOne
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+userTwo.email = "goswami@google.com"
+
+console.log(userTwo.email);
+console.log(userOne.email);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// This is called stack memory copy of value not address(Value not changed).
+
+let anotherId2 = "Astik"
+let anotherName = anotherId2
+
+anotherName = "Monisha"
+
+console.log(anotherId2);
+console.log(anotherName);
+
+console.log(typeof console);
+
+
