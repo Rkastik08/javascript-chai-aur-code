c8c8c4af0ebe0114af9d05b334ea65be96241f89
Astik08 <astikgoswami2004@gmail.com>
2024-10-06 02:07:11 +0530

    Add files via upload

---
+++ 05_iterations/five.js
@@ -0,0 +1,13 @@
+//  for each loop
+
+const prog = ["js", "cpp", "rb", "swift"]
+
+prog.forEach(function (langs) {
+  console.log(langs);
+
+})// forEach loop attribute function doesnt have a name
+console.log();
+
+prog.forEach( (langs) => {console.log(langs)} )
+
+log
